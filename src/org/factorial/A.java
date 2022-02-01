a81060ebc90925e602a3b7c9783b6ac0ff77150d
krkk12 <kumaranprathu12@gmail.com>
2022-02-01 16:36:02 +0530

    message

---
+++ src/org/factorial/A.java
@@ -0,0 +1,19 @@
+package org.factorial;
+
+public class A {
+	public static void main(String[] args) {
+
+		int num = 4;
+		int fac = 1;
+
+		for (int i = 1; i <= 4; ++i) {
+			
+			fac *= i;
+			
+			
+		}
+		System.out.println(fac);
+
+	}
+
+}
